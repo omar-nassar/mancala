bb49140960244d85db7615e6270e7896d7c65ccd
Omar.Nassar <omr.nassar@gmail.com>
2021-09-04 20:26:37 +0300

    removing board repository

--- src/main/java/io/nassar/game/mancala/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package io.nassar.game.mancala.repository;
-
-import io.nassar.game.mancala.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
